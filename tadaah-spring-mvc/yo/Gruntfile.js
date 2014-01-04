16264c018d5cab5d1c1d940e6a1db2d4b4e15b9f
Nicolas Hurion <nicolas@hurion.eu>
2014-01-04 15:24:00 +0100

    disable karma for the moment, need to figure out how to run firefox headless

--- tadaah-spring-mvc/yo/Gruntfile.js
+++ tadaah-spring-mvc/yo/Gruntfile.js
@@ -376,8 +376,8 @@ module.exports = function (grunt) {
     'clean:server',
     'concurrent:test',
     'autoprefixer',
-    'connect:test',
-    'karma'
+    'connect:test'//,
+    //'karma'
   ]);
 
   grunt.registerTask('build', [
