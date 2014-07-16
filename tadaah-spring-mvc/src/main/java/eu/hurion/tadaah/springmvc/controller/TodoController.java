ec2caa8f52a77efbbb437351ee7fc8da33b0f6bf
Nicolas Hurion <nicolas@hurion.eu>
2014-07-16 18:53:36 +0200

    fix new url

--- tadaah-spring-mvc/src/main/java/eu/hurion/tadaah/springmvc/controller/TodoController.java
+++ tadaah-spring-mvc/src/main/java/eu/hurion/tadaah/springmvc/controller/TodoController.java
@@ -3,17 +3,17 @@ package eu.hurion.tadaah.springmvc.controller;
 import eu.hurion.tadaah.model.Todo;
 import eu.hurion.tadaah.service.TodoRegistry;
 import eu.hurion.tadaah.springmvc.dto.TodoDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class TodoController {
 
-    @Autowired
+    @Inject
     private TodoRegistry todoRegistry;
 
     @RequestMapping(value = "/todo", method = RequestMethod.GET)
